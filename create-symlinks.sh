80f297246a6b2aebaec10536cffa0307b42aa42d
Patrick Kerwood <patrick@kerwood.dk>
2022-02-14 21:12:33 +0100

    Added create-symlinks.sh

---
+++ create-symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p ~/Pictures
+ln -s $(pwd)/config/alacritty ~/.config/alacritty
+ln -s $(pwd)/config/autorandr ~/.config/autorandr
+ln -s $(pwd)/config/i3 ~/.config/i3
+ln -s $(pwd)/config/polybar ~/.config/polybar
+ln -s $(pwd)/background.jpg ~/Pictures/background.jpg
