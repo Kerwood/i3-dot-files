e5234c397c1b6e6e69890f835ac69310e2f30979
Patrick Kerwood <patrick@kerwood.dk>
2022-03-15 18:17:26 +0100

    Changed polybar launch script

--- config/polybar/polybar-launch.sh
+++ config/polybar/polybar-launch.sh
@@ -8,12 +8,9 @@ killall -q polybar
 # Launch bar1 and bar2
 polybar top-primary 2>&1 | tee -a /tmp/polybar-primary.log & disown
 
-if [ $(xrandr --query | grep DP-1-8 | wc -l) -eq 1 ]; then 
-  polybar DP-1-8 2>&1 | tee -a /tmp/polybar-DP-1-8-2.log & disown
-fi
-
-if [ $(xrandr --query | grep DP-2-3-8 | wc -l) -eq 1 ]; then 
+if [ $(xrandr --query | grep DP-2-3-8 | wc -l) -eq 1 && $(xrandr --query | grep DP-1-8 | wc -l) -eq 1 ]; then 
   polybar DP-2-3-8 2>&1 | tee -a /tmp/polybar-DP-2-3-8.log & disown
+  polybar DP-1-8 2>&1 | tee -a /tmp/polybar-DP-1-8-2.log & disown
 fi
 
 echo "Bars launched..."
