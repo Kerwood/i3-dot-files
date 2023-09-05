9ccc53dde7a9015a1a8674788769a5876e95a761
Patrick Kerwood <patrick@kerwood.dk>
2023-09-05 19:38:14 +0200

    updated i3 config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,4 +4,5 @@ ln -s $(pwd)/config/alacritty ~/.config/alacritty
 ln -s $(pwd)/config/autorandr ~/.config/autorandr
 ln -s $(pwd)/config/i3 ~/.config/i3
 ln -s $(pwd)/config/polybar ~/.config/polybar
+ln -s $(pwd)/config/udiskie ~/.config/udiskie
 ln -s $(pwd)/background.jpg ~/Pictures/background.jpg
